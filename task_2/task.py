7aff94b807ea83db8255d89ba7e23e011ad526ce
Karina1996 <69156101+Karina1996@users.noreply.github.com>
2023-03-24 21:27:07 +0300

    Update task.py

--- task_2/task.py
+++ task_2/task.py
@@ -8,19 +8,17 @@ import imaplib
 import os
 import base64
 
-global letter
-global sign
+
 # модуль 1 - отправляет письм по эл. почте с ЭЦП
 def send_email_with_encrypted_file(recipient_email, sender_email, sender_email_password, pubkey, privkey):
     data = b'open message'
     # генерация ЭЦП
     signature = rsa.sign(data, privkey, 'SHA-256')
+    encrypted_aes_key = rsa.encrypt(data, pubkey) # Зашифровываем ключ и передаем для расшифровки большого сообщения.
     # Отправляем открытый текст и ЭЦП по email
-    # msg = email.message.Message()
-    msg = MIMEMultipart()                               # Создаем сообщение
-    # msg.set_payload('-----' + str(data) + '-----' + str(signature))
-    letter = msg.attach(MIMEText(str(data) + '\n', 'plain'))                 
-    sign = msg.attach(MIMEText(str(signature), 'plain'))  
+    msg = MIMEMultipart()                             
+    letter = msg.attach(MIMEText(str(data), 'plain'))                 
+    sign = msg.attach(MIMEText(str(encrypted_aes_key), 'plain'))  
     msg['From'] = sender_email                              # Адресат
     msg['To'] = recipient_email                             # Получатель
     smtp_server = smtplib.SMTP('smtp.gmail.com', 587)       # Создаем объект SMTP
@@ -43,16 +41,20 @@ def receive_and_decrypt_email(recipient_email, recipient_email_password, pubkey,
     result, data = imap_server.fetch(latest_email_id, '(RFC822)')
     raw_email = data[0][1]
     email_message = email.message_from_bytes(raw_email)
-    data = email_message.get_payload(0)
-    sign = email_message.get_payload(1)
+    
+    ls = []
+    for part in email_message.walk():
+        if part.get_content_type() == 'text/plain':
+            ls.append(part.get_payload())
     try:
-        rsa.verify(data, sign, pubkey) # передать текст письма и ЭЦП
+        rsa.verify(str(ls[0]) + str(ls[-1]), ls[-1], pubkey) # передать текст письма и ЭЦП
         print('Письмо имеет действительную подпись!')
     except rsa.pkcs1.VerificationError:
         print('Подпись недействительна!')
 
+
 if __name__ == "__main__":
-    (pubkey, privkey) = rsa.newkeys(512)  # Генерируем ключи RSA
+    (pubkey, privkey) = rsa.newkeys(1024)  # Генерируем ключи RSA
     send_email_with_encrypted_file('lazarevak1996@gmail.com', 'lazarevak1996@gmail.com', '', pubkey, privkey) 
     receive_and_decrypt_email('lazarevak1996@gmail.com', '', pubkey, privkey)
 
